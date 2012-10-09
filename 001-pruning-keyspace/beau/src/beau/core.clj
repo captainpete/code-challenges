dff7a1d3b90c511991d95c390cde448eec4be5f2
Beau Fabry <beau.fabry@c3businesssolutions.com>
2012-10-09 12:17:42 +1100

    takes quite a bit longer since I started following the rules

--- 001-pruning-keyspace/beau/src/beau/core.clj
+++ 001-pruning-keyspace/beau/src/beau/core.clj
@@ -3,24 +3,28 @@
 (def possibles '(A B C D E F 1 2 3 4 5 6 7 8 9))
 
 (defn filter-by-previous [prev-char]
+  (remove #{prev-char} possibles))
+
+(defn filter-by-previous2 [prev-char second-last-char]
   (filter (fn [possible]
             (cond
-              (and (number? prev-char) (number? possible)) false
-              (and (symbol? prev-char) (symbol? possible)) false
-              (= prev-char possible) false
+              (every? number? [prev-char second-last-char possible]) false
+              (every? symbol? [prev-char second-last-char possible]) false
               :else true))
-          possibles))
+          (filter-by-previous prev-char)))
+
+(def fast-filter (memoize filter-by-previous2))
 
 (defn space []
   (filter #(some symbol? %)
-          (apply interleave
+          (reduce concat
                  (pmap (fn [char1] (for [char2 (filter-by-previous char1)
-                                         char3 (filter-by-previous char2)
-                                         char4 (filter-by-previous char3)
-                                         char5 (filter-by-previous char4)
-                                         char6 (filter-by-previous char5)
-                                         char7 (filter-by-previous char6)
-                                         char8 (filter-by-previous char7)]
+                                         char3 (fast-filter char2 char1)
+                                         char4 (fast-filter char3 char2)
+                                         char5 (fast-filter char4 char3)
+                                         char6 (fast-filter char5 char4)
+                                         char7 (fast-filter char6 char5)
+                                         char8 (fast-filter char7 char6)]
 
                                      [char1 char2 char3 char4 char5 char6 char7 char8]))
                        possibles))))
